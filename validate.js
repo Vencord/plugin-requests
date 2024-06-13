a5b0357e2cc0f976408e14cff31b334cf51af69d
Saahil <neon@saahild.com>
2024-06-13 15:21:31 -0400

    Done v2

--- validate.js
+++ validate.js
@@ -1,15 +1,21 @@
-// debyg
-console.log(process.env)
+
 // an array with all betterDiscordPlugins which end up being banned
 const betterDiscordPlugins = [
-
+"https://betterdiscord.app/plugin/FavoriteMedia", // https://github.com/Vencord/plugin-requests/issues/716
+"https://github.com/Eidenz/XSOverlay-BetterDiscord",
+"https://betterdiscord.app/plugin/ServerCounter",
+"https://betterdiscord.app/plugin/RemoveBlockedUsers"
 ]
 const words = [
-  'close-me-if-i-have-this-word',
+// test param
+    'close-me-if-i-have-this-word',
+// extracted from issue template
+"Stereo Mic",
+"FxTwitter",
+"FakeDeafen",
   ...betterDiscordPlugins
-]
+].map(e => e.toLowerCase().trim())
 const ghApiReq = (path, body) => {
-  console.log(`https://api.github.com/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/${path}`)
   return fetch(`https://api.github.com/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/${path}`, {
     headers: {
       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
@@ -23,7 +29,7 @@ const ghApiReq = (path, body) => {
 }
 const allContent = process.env.CONTENT
 
-if (words.some(w => allContent.includes(w))) {
+if (words.some(w => allContent.toLowerCase().includes(w))) {
   console.log(`A word was found in the issue.`)
   ghApiReq(`issues/${process.env.ISSUE_NUMBER}`, {
     state: 'closed',
